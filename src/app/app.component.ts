08c2b9fb288ebf423ab16e94919b805789bb1e8c
Zacfou <36445119+Zacfou@users.noreply.github.com>
2020-12-23 16:01:05 +0100

    first chapitre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Tutoriel Angular';
+  isAuth = false;
+
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+      );
+    }
+  );
+
+  appareils = [
+  	{
+  		name: 'Machine à laver',
+  		status: 'éteint'
+  	},
+  	{
+  		name: 'Télévision',
+  		status: 'allumé'
+  	},
+  	{
+  		name: 'Frigot',
+  		status: 'éteint'
+  	}
+  ];
+
+  /*appareilOne = "Machine à laver";
+  appareilTwo = "Télévision";
+  appareilThree = "Fr";*/
+  constructor() {
+  	setTimeout(
+  		() => {
+  			this.isAuth = true;
+  		}, 4000
+  	);
+  }
+  onAllumer() {
+  	console.log('On allume tout !');
+  }
 }
